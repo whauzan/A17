8581c73014404229a10065efbb75f9b3abcdc93c
Wahyu Hauzan Rafi <57913209+whauzan@users.noreply.github.com>
2021-08-12 11:15:44 +0700

    almost

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,20 @@
+// hamburger menu
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', function() {
+    nav.classList.toggle('slide');
+});
+// akhir
+
+// scrolling
+window.addEventListener('scroll', function () {
+    let header = document.querySelector('nav');
+
+    header.classList.toggle('scrolling', window.scrollY > 0);
+});
+// akhir
+
 const next = document.querySelector('.next');
 const prev = document.querySelector('.prev');
 const slides = document.querySelectorAll('.slide');
